fc305579a855d24fc4a2b8c08c2cbb6e6847c222
Khoa Nguyen <khoa02031995@gmail.com>
2015-11-27 03:14:34 +0700

    Refactor login form

--- app/components/Application/index.jsx
+++ app/components/Application/index.jsx
@@ -5,31 +5,75 @@ import styles from './style';
 import avatar from './img/avatar.jpg';
 
 class Application extends React.Component {
-  construct() {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      name: 'Khoa',
+      email: 'khoa02031995@gmail.com',
+      code: 'haha'
+    }
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handleCodeChange = this.handleCodeChange.bind(this)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    alert(this.state.code)
+  }
+
+  handleNameChange = (e) => {
+    this.setState({email: e.target.value})
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({email: e.target.value})
+  }
+
+  handleCodeChange = (e) => {
+    this.setState({code: e.target.value})
   }
 
   render() {
     return <div className="container">
       <div className="row">
-      <div className="col-sm-8 col-md-5" styleName="loginContainer">
-        <div id="output"></div>
-        <div styleName="avatar">
-          <img src={avatar}></img>
-          <h2>Enter your details</h2>
-        </div>
-        <div styleName="formBox">
-          <form action="" method="">
-            <input type="text" placeholder="Your name" require></input>
-            <input type="email" placeholder="Your email" require></input>
-            <input type="text" placeholder="Your login code" require></input>
-            <button className="btn btn-info btn-block" styleName="formBoxLogin" type="submit">Go</button>
-          </form>
+        <div className="col-sm-8 col-md-5" styleName="loginContainer">
+          <div id="output"></div>
+          <div styleName="avatar">
+            <img src={avatar}></img>
+            <h2>Enter your details</h2>
+          </div>
+          <div styleName="formBox">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text" placeholder="Your name"
+                value={this.state.name}
+                onChange={this.handleNameChange.bind(this)}
+                require
+                ></input>
+              <input
+                type="email" placeholder="Your email"
+                value={this.state.email}
+                onChange={this.handleEmailChange.bind(this)}
+                require
+                ></input>
+              <input
+                type="text" placeholder="Your code"
+                value={this.state.code}
+                onChange={this.handleCodeChange.bind(this)}
+                require
+                ></input>
+              <button
+                type="submit"
+                className="btn btn-info btn-block" styleName="formBoxLogin"
+              >Go</button>
+            </form>
+          </div>
         </div>
       </div>
-        </div>
     </div>;
   }
 }
 
-export default CSSModules(Application, styles);
+export default CSSModules(Application, styles)
